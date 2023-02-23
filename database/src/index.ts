66f0c21dc8453950cc0c4484d63e8b03d1b9cf80
Marián Lalík <marian.lalik@skautocont.onmicrosoft.com>
2023-02-23 15:14:47 +0100

    try catch for requests + appropriate responses

--- database/src/index.ts
+++ database/src/index.ts
@@ -1,5 +1,7 @@
 import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import express, {Express, Request, Response} from "express"
+import { title } from 'process'
 
 const prisma = new PrismaClient()
 const app: Express = express()
@@ -10,56 +12,121 @@ export interface TPRequest<T> extends Express.Request { params: T }
 app.use(express.json())
 
 app.get('/', async (req: Request, res: Response) => {
-  res.json("Welcome to my API!")
+  res.status(200).json("Welcome to my API!")
 })
 
 app.get("/ListItem/:id", async (req: TPRequest<{ id: string }>, res: Response) => {
-  const result = await prisma.listItem.findUnique({
-    where: {
-      id: req.params.id,
-    },
-  })
-  res.json(result)
+  try{
+    const result = await prisma.listItem.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(result)
+  }
+  catch (error: any) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.get("/ListItem", async (req: TBRequest<{ id?: string, title?: string }>, res: Response) => {
-  const result = await prisma.listItem.findMany({
-    where: {
-      ...(req.body.id ? {id: req.body.id} : {}),
-      ...(req.body.title ? {title: req.body.title} : {})
-    }
-  })
-  res.json(result)
+  try {
+    const result = await prisma.listItem.findMany({
+      where: {
+        ...(req.body.id ? {id: req.body.id} : {}),
+        ...(req.body.title ? {title: req.body.title} : {})
+      }
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.delete("/ListItem/:id", async (req: TPRequest<{ id: string }>, res: Response) => {
-  const result = await prisma.listItem.delete({
-    where: {
-      id: req.params.id,
-    },
-  })
-  res.json(result)
+  try {
+    const check = await prisma.listItem.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if (!check) {
+      res.status(200).json(null)
+      return
+    }
+    const result = await prisma.listItem.delete({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.post("/ListItem", async (req: TBRequest<{ title: string }>, res: Response) => {
-  const result = await prisma.listItem.create({
-    data: {
-      title: req.body.title
-    },
-  })
-  res.json(result)
+  try {
+    if (!req.body.title) {
+      res.status(400).json({
+        message: `Bad Request: Missing attribute "title"`,
+      })
+      return
+    }
+    const result = await prisma.listItem.create({
+      data: {
+        title: req.body.title
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.patch("/ListItem", async (req: TBRequest<{ id: string, title?: string }>, res: Response) => {
-  const result = await prisma.listItem.update({
-    where: {
-      id: req.body.id
-    },
-    data: {
-      ...(req.body.title ? {title: req.body.title} : {})
-    },
-  })
-  res.json(result)
+  try {
+    if(!req.body.id){
+      res.status(400).json({
+        message: `Bad Request: Missing attribute "id"`,
+      })
+      return
+    }
+    const check = await prisma.listItem.findUnique({
+      where: {
+        id: req.body.id,
+      },
+    })
+    if (!check) {
+      res.status(200).json(null)
+      return
+    }
+    const result = await prisma.listItem.update({
+      where: {
+        id: req.body.id
+      },
+      data: {
+        ...(req.body.title ? {title: req.body.title} : {})
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.options("/ListItem", async (req: Request, res: Response) => {
@@ -69,55 +136,133 @@ app.options("/ListItem", async (req: Request, res: Response) => {
 })
 
 app.get("/ListRelatedItem/:id", async (req: TPRequest<{ id: string }>, res: Response) => {
-  const result = await prisma.listRelatedItem.findUnique({
-    where: {
-      id: req.params.id,
-    },
-  })
-  res.json(result)
+  try {
+    const result = await prisma.listRelatedItem.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.get("/ListRelatedItem", async (req: TBRequest<{ id?: string, title?: string, relatedId?: string }>, res: Response) => {
-  const result = await prisma.listRelatedItem.findMany({
-    where: {
-      ...(req.body.id ? {id: req.body.id} : {}),
-      ...(req.body.title ? {title: req.body.title} : {}),
-      ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
-    }
-  })
-  res.json(result)
+  try {
+    const result = await prisma.listRelatedItem.findMany({
+      where: {
+        ...(req.body.id ? {id: req.body.id} : {}),
+        ...(req.body.title ? {title: req.body.title} : {}),
+        ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
+      }
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.delete("/ListRelatedItem/:id", async (req: TPRequest<{ id: string }>, res: Response) => {
-  const result = await prisma.listRelatedItem.delete({
-    where: {
-      id: req.params.id,
-    },
-  })
-  res.json(result)
+  try {
+    const check = await prisma.listRelatedItem.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    })
+    if (!check) {
+      res.status(200).json(null)
+      return
+    }
+    const result = await prisma.listRelatedItem.delete({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.post("/ListRelatedItem", async (req: TBRequest<{ title: string, relatedId?: string }>, res: Response) => {
-  const result = await prisma.listRelatedItem.create({
-    data: {
-      title: req.body.title,
-      ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
-    },
-  })
-  res.json(result)
+  try {
+    if (!req.body.title) {
+      res.status(400).json({
+        message: `Bad Request: Missing attribute "title"`,
+      })
+      return
+    }
+    const result = await prisma.listRelatedItem.create({
+      data: {
+        title: req.body.title,
+        ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.patch("/ListRelatedItem", async (req: TBRequest<{ id: string, title?: string, relatedId?: string }>, res: Response) => {
-  const result = await prisma.listRelatedItem.update({
-    where: {
-      id: req.body.id
-    },
-    data: {
-      ...(req.body.title ? {title: req.body.title} : {}),
-      ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
-    },
-  })
-  res.json(result)
+  try {
+    if(!req.body.id){
+      res.status(400).json({
+        message: `Bad Request: Missing attribute "id"`,
+      })
+      return
+    }
+    const check = await prisma.listRelatedItem.findUnique({
+      where: {
+        id: req.body.id,
+      },
+    })
+    if (!check) {
+      res.status(200).json(null)
+      return
+    }
+    if(req.body.relatedId){
+      const checkRelated = await prisma.listItem.findUnique({
+        where: {
+          id: req.body.relatedId,
+        },
+      })
+      if (!checkRelated) {
+        res.status(400).json({
+          message: `Bad Request: ListItem with Id "${req.body.relatedId}" does not exist`,
+        })
+        return
+      }
+    }
+    const result = await prisma.listRelatedItem.update({
+      where: {
+        id: req.body.id
+      },
+      data: {
+        ...(req.body.title ? {title: req.body.title} : {}),
+        ...(req.body.relatedId ? {relatedId: req.body.relatedId} : {})
+      },
+    })
+    res.json(result)
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    })
+  }
 })
 
 app.options("/ListRelatedItem", async (req: Request, res: Response) => {
